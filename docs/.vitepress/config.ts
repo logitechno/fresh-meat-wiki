c26cfacc9eb222e15e8612911bfd9ed7bef7bc71
logitechno <soroki1n62@gmail.com>
2024-11-25 17:43:32 +0300

    Update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/fresh-meat-wiki/',
   head: [["link", { rel: "icon", href: "/logo.png" }]],
   cleanUrls: true,
   lang: 'ru-RU',
